d4feec7e976fce9c292f8142396471012855ac17
M-auro <mauromorrati@outlook.com>
2025-02-21 09:05:29 +0000

    Enhance documentation in variables.py with module description and improve comment clarity

--- week1/variables.py
+++ week1/variables.py
@@ -1,17 +1,21 @@
+"""
+This module demonstrates basic variable usage in Python.
+It includes examples of variable assignment, printing, and multiple assignments.
+"""
 
 # Variables are used to store values, and they are created when you assign a value to it.
-# Example of variableS
+# Example of variables
 
 X = 10           # Integer
 Y = 3.14         # Float
-NAME = "frodo"   # string
+NAME = "frodo"   # String
 IS_COOL = True   # Boolean
 
-# To print the value of a variable, you can use the print
+# To print the value of a variable, you can use the print function
 print(X)
 
 # You can also assign multiple variables in one line
-A, B, C = 1, 2, 3 
+A, B, C = 1, 2, 3
 
 # You can also assign the same value to multiple variables in one line
 D = E = F = 4
