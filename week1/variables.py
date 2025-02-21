85e739cd523e73db143f36c66d5ce29d66f42ff2
M-auro <mauromorrati@outlook.com>
2025-02-21 09:01:48 +0000

    Update variable names in variables.py to follow consistent uppercase naming convention

--- week1/variables.py
+++ week1/variables.py
@@ -2,20 +2,20 @@
 # Variables are used to store values, and they are created when you assign a value to it.
 # Example of variableS
 
-x = 10           # Integer
-y = 3.14         # Float
-name = "frodo"   # string
-is_cool = True   # Boolean
+X = 10           # Integer
+Y = 3.14         # Float
+NAME = "frodo"   # string
+IS_COOL = True   # Boolean
 
 # To print the value of a variable, you can use the print
 print(x)
 
 # You can also assign multiple variables in one line
-a, b, c = 1, 2, 3 
+A, B, C = 1, 2, 3 
 
 # You can also assign the same value to multiple variables in one line
-d = e = f = 4
+D = E = F = 4
 
 # You can also assign a variable to another variable
-g = h = 5
-g = h
+G = H = 5
+G = H
