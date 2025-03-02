4a2ba2111caa970a446a08575f4bd8d92133ed4f
M-auro <mauromorrati@outlook.com>
2025-03-02 15:35:37 +0000

    Refactor project2.py documentation and function parameters for clarity

--- week2/project2.py
+++ week2/project2.py
@@ -1,23 +1,21 @@
-# Write a script that list running k8s pods in a given namespace using the Kubernetes API in Python.
-#
-# The script should have the following functions:
-#
-# list_pods(namespace)
-# This function should list all running pods in the given namespace.
-#
-# The script should be able to run as a standalone script and list all running pods:
-# In the default.
-# In a given namespace
+"""
+This script lists running Kubernetes pods in a given namespace using the Kubernetes API in Python.
 
+Functions:
+- list_running_pods(namespace): Lists all running pods in the given namespace.
 
+The script can run as a standalone script and list all running pods:
+- In the default namespace.
+- In a given namespace specified by the KUBERNETES_NAMESPACE environment variable.
+"""
 
 import os
-from kubernetes import client, config # Import the required libraries
+from kubernetes import client, config  # Import the required libraries
 
-def list_running_pods (namespace):
+def list_running_pods(ns):
     """
     List all running pods in a given namespace.
-    :param namespace: Kubernetes namespace
+    :param ns: Kubernetes namespace
     """
     # Load the Kubernetes configuration file
     config.load_kube_config()
@@ -26,12 +24,12 @@ def list_running_pods (namespace):
     v1 = client.CoreV1Api()
 
     # List all running pods in the given namespace
-    pods = v1.list_namespaced_pod(namespace)
+    pods = v1.list_namespaced_pod(ns)
 
     # Get a list of all running pod names from the response
     pod_names = [pod.metadata.name for pod in pods.items]
 
-    print(f"Running Pods in Namespace {namespace}: {pod_names}")
+    print(f"Running Pods in Namespace {ns}: {pod_names}")
 
 if __name__ == "__main__":
     # Check if the KUBERNETES_NAMESPACE environment variable is set
@@ -40,4 +38,4 @@ if __name__ == "__main__":
     if namespace:
         list_running_pods(namespace)
     else:
-        list_running_pods("default") # List all running pods in the default namespace
+        list_running_pods("default")  # List all running pods in the default namespace
